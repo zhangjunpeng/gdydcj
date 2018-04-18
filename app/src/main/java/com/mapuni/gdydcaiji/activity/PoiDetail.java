df7cfb9afef789088e153e43a78efa8a6d961771
yf <18317858202@183.com>
2018-04-18 11:29:46 +0800

    更改poi的type、性质样式

--- app/src/main/java/com/mapuni/gdydcaiji/activity/PoiDetail.java
+++ app/src/main/java/com/mapuni/gdydcaiji/activity/PoiDetail.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -37,13 +36,9 @@ import butterknife.ButterKnife;
  * poi采集
  */
 
-public class PoiDetail extends BaseDetailActivity<TbPoint> {
+public class PoiDetail extends BaseDetailActivity<TbPoint> implements View.OnClickListener {
     @BindView(R.id.tv_title)
     TextView title;
-    @BindView(R.id.sp_lyType)
-    Spinner spLyType;
-    @BindView(R.id.sp_lyxz)
-    Spinner spLyxz;
     @BindView(R.id.et_lyName)
     AutoCompleteTextView etLyName;
     @BindView(R.id.sp_lyfl)
@@ -64,9 +59,23 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
     ImageView ivCalculator;
     @BindView(R.id.et_bz)
     ClearEditText etBz;
+    @BindView(R.id.tv_type0)
+    TextView tvType0;
+    @BindView(R.id.tv_type1)
+    TextView tvType1;
+    @BindView(R.id.tv_type2)
+    TextView tvType2;
+    @BindView(R.id.tv_type3)
+    TextView tvType3;
+    @BindView(R.id.tv_xz0)
+    TextView tvXz0;
+    @BindView(R.id.tv_xz1)
+    TextView tvXz1;
+    @BindView(R.id.tv_xz2)
+    TextView tvXz2;
 
     private TbPointDao tPoiInfoDao;
-//    private List<String> mAddArray = new ArrayList<>();
+    private String lyType = "", lyXz = "";
 //    private String address;
 
     @Override
@@ -80,13 +89,21 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
 
         title.setText("POI点采集");
         tPoiInfoDao = GdydApplication.getInstances().getDaoSession().getTbPointDao();
-        setSpinnerData(R.array.building_types, spLyType);
-        setSpinnerData(R.array.building_xz, spLyxz);
+//        setSpinnerData(R.array.building_types, spLyType);
+//        setSpinnerData(R.array.building_xz, spLyxz);
         setSpinnerData(R.array.building_fl, spLyfl);
         setSpinnerData(R.array.poi_mjdj, spDj);
         spLyfl.setSelection(getResources().getStringArray(R.array.building_fl).length - 1);
         initListPopupWindow();
 
+        tvType0.setText(getResources().getStringArray(R.array.building_types)[0]);
+        tvType1.setText(getResources().getStringArray(R.array.building_types)[1]);
+        tvType2.setText(getResources().getStringArray(R.array.building_types)[2]);
+        tvType3.setText(getResources().getStringArray(R.array.building_types)[3]);
+
+        tvXz0.setText(getResources().getStringArray(R.array.building_xz)[0]);
+        tvXz1.setText(getResources().getStringArray(R.array.building_xz)[1]);
+        tvXz2.setText(getResources().getStringArray(R.array.building_xz)[2]);
     }
 
     private void initListPopupWindow() {
@@ -126,14 +143,27 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
                 openJS();
             }
         });
+
+        tvType0.setOnClickListener(this);
+        tvType1.setOnClickListener(this);
+        tvType2.setOnClickListener(this);
+        tvType3.setOnClickListener(this);
+
+        tvXz0.setOnClickListener(this);
+        tvXz1.setOnClickListener(this);
+        tvXz2.setOnClickListener(this);
     }
 
     @Override
     protected void showData() {
         etLyName.setText(resultBean.getName());
         etAddress.setText(resultBean.getDz());
-        spLyType.setSelection(TextUtils.isEmpty(resultBean.getLytype()) ? 0 : getSelectPosition(R.array.building_types, resultBean.getLytype()));
-        spLyxz.setSelection(TextUtils.isEmpty(resultBean.getLyxz()) ? 0 : getSelectPosition(R.array.building_xz, resultBean.getLyxz()));
+//        spLyType.setSelection(TextUtils.isEmpty(resultBean.getLytype()) ? 0 : getSelectPosition(R.array.building_types, resultBean.getLytype()));
+        lyType = resultBean.getLytype();
+        setSelecteTypeTv(lyType);
+        lyXz = resultBean.getLyxz();
+        setSelecteLyXzTv(lyXz);
+//        spLyxz.setSelection(TextUtils.isEmpty(resultBean.getLyxz()) ? 0 : getSelectPosition(R.array.building_xz, ));
         spLyfl.setSelection(getSelectPosition(R.array.building_fl, resultBean.getFl()));
         spDj.setSelection(TextUtils.isEmpty(resultBean.getDj()) ? 0 : getSelectPosition(R.array.poi_mjdj, resultBean.getDj()));
         etDyh.setText(resultBean.getDy());
@@ -162,6 +192,7 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
         super.showData();
     }
 
+
     @Override
     protected void submit() {
         if (resultBean == null) {
@@ -172,8 +203,10 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
         }
 
         resultBean.setName(getTextByView(etLyName));
-        resultBean.setLytype(spLyType.getSelectedItemPosition() == 0 ? "" : getResources().getStringArray(R.array.building_types)[spLyType.getSelectedItemPosition()]);
-        resultBean.setLyxz(spLyxz.getSelectedItemPosition() == 0 ? "" : getResources().getStringArray(R.array.building_xz)[spLyxz.getSelectedItemPosition()]);
+//        resultBean.setLytype(spLyType.getSelectedItemPosition() == 0 ? "" : getResources().getStringArray(R.array.building_types)[spLyType.getSelectedItemPosition()]);
+        resultBean.setLytype(lyType);
+//        resultBean.setLyxz(spLyxz.getSelectedItemPosition() == 0 ? "" : getResources().getStringArray(R.array.building_xz)[spLyxz.getSelectedItemPosition()]);
+        resultBean.setLyxz(lyXz);
         resultBean.setFl(getResources().getStringArray(R.array.building_fl)[spLyfl.getSelectedItemPosition()]);
         resultBean.setDz(getTextByView(etAddress));
         resultBean.setDy(getTextByView(etDyh));
@@ -251,4 +284,137 @@ public class PoiDetail extends BaseDetailActivity<TbPoint> {
         return null;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_type0:
+                if (lyType.equals(getResources().getStringArray(R.array.building_types)[0])) {
+                    setNormalColor(tvType0);
+                    lyType = "";
+                } else {
+                    setselecteFirstTv(tvType0, tvType1, tvType2, tvType3);
+                    lyType = getResources().getStringArray(R.array.building_types)[0];
+                }
+                break;
+            case R.id.tv_type1:
+                if (lyType.equals(getResources().getStringArray(R.array.building_types)[1])) {
+                    setNormalColor(tvType1);
+                    lyType = "";
+                } else {
+                    setselecteFirstTv(tvType1, tvType0, tvType2, tvType3);
+                    lyType = getResources().getStringArray(R.array.building_types)[1];
+                }
+                break;
+            case R.id.tv_type2:
+                if (lyType.equals(getResources().getStringArray(R.array.building_types)[2])) {
+                    setNormalColor(tvType2);
+                    lyType = "";
+                } else {
+                    setselecteFirstTv(tvType2, tvType0, tvType1, tvType3);
+                    lyType = getResources().getStringArray(R.array.building_types)[2];
+                }
+                break;
+            case R.id.tv_type3:
+                if (lyType.equals(getResources().getStringArray(R.array.building_types)[3])) {
+                    setNormalColor(tvType3);
+                    lyType = "";
+                } else {
+                    setselecteFirstTv(tvType3, tvType0, tvType1, tvType2);
+                    lyType = getResources().getStringArray(R.array.building_types)[3];
+                }
+                break;
+            case R.id.tv_xz0:
+                if (lyXz.equals(getResources().getStringArray(R.array.building_xz)[0])) {
+                    setNormalColor(tvXz0);
+                    lyXz = "";
+                } else {
+                    setselecteFirstTv(tvXz0, tvXz1, tvXz2);
+                    lyXz = getResources().getStringArray(R.array.building_xz)[0];
+                }
+                break;
+            case R.id.tv_xz1:
+                if (lyXz.equals(getResources().getStringArray(R.array.building_xz)[1])) {
+                    setNormalColor(tvXz1);
+                    lyXz = "";
+                } else {
+                    setselecteFirstTv(tvXz1, tvXz0, tvXz2);
+                    lyXz = getResources().getStringArray(R.array.building_xz)[1];
+                }
+                break;
+            case R.id.tv_xz2:
+                if (lyXz.equals(getResources().getStringArray(R.array.building_xz)[2])) {
+                    setNormalColor(tvXz2);
+                    lyXz = "";
+                } else {
+                    setselecteFirstTv(tvXz2, tvXz0, tvXz1);
+                    lyXz = getResources().getStringArray(R.array.building_xz)[2];
+                }
+                break;
+        }
+    }
+
+    private void setNormalColor(TextView tv) {
+        tv.setTextColor(getResources().getColor(R.color.black_text));
+        tv.setBackgroundResource(R.drawable.poi_tv_bg_normal);
+    }
+
+    private void setselecteColor(TextView tv) {
+        tv.setTextColor(getResources().getColor(R.color.white));
+        tv.setBackgroundResource(R.drawable.poi_tv_bg_selector);
+    }
+
+    private void setselecteFirstTv(TextView... tv) {
+        if (tv.length == 0) {
+            return;
+        }
+        setselecteColor(tv[0]);
+        for (int i = 1; i < tv.length; i++) {
+            setNormalColor(tv[i]);
+        }
+    }
+
+    private void setSelecteTypeTv(String s) {
+        if (TextUtils.isEmpty(s)) {
+            lyType = "";
+            return;
+        }
+
+        int selectPosition = getSelectPosition(R.array.building_types, s);
+        switch (selectPosition) {
+            case 0:
+                setselecteFirstTv(tvType0, tvType1, tvType2, tvType3);
+                break;
+            case 1:
+                setselecteFirstTv(tvType1, tvType0, tvType2, tvType3);
+                break;
+            case 2:
+                setselecteFirstTv(tvType2, tvType0, tvType1, tvType3);
+                break;
+            case 3:
+                setselecteFirstTv(tvType3, tvType0, tvType1, tvType2);
+                break;
+
+        }
+
+    }
+
+    private void setSelecteLyXzTv(String s) {
+        if (TextUtils.isEmpty(s)) {
+            lyXz = "";
+            return;
+        }
+
+        int selectPosition = getSelectPosition(R.array.building_xz, s);
+        switch (selectPosition) {
+            case 0:
+                setselecteFirstTv(tvXz0, tvXz1, tvXz2);
+                break;
+            case 1:
+                setselecteFirstTv(tvXz1, tvXz0, tvXz2);
+                break;
+            case 2:
+                setselecteFirstTv(tvXz2, tvXz0, tvXz1);
+                break;
+        }
+    }
 }
